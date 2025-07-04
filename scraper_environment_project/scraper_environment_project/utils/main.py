f15bfde2373fe1c1ef66312bff29fc3d17aab438
shanvinluocima <shanvin.luo@cima.ca>
2025-07-04 10:15:54 -0400

    Refactored packages into folders and take out all useless __init.py__

--- scraper_environment_project/scraper_environment_project/utils/main.py
+++ scraper_environment_project/scraper_environment_project/utils/main.py
@@ -3,5 +3,6 @@ from pathlib import Path
 
 if __name__ == '__main__':
     strategy = REAFIE_extraction()
-    result = strategy.extract(Path("../data/html/REAFIE_20250703_134550.html"), Path("../knowledge_base_data/REAFIE.txt"))
+    result = strategy.extract(Path("../data/html/REAFIE_20250704_101328.html"), Path(
+        "../knowledge_base_data/REAFIE.txt"))
     print("Extracted content length:", len(result))
